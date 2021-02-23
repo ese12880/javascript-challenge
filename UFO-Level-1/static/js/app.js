1a40ebad2d3d8e71937a67fae0510e3ad5ed2639
ese12880 <70610771+ese12880@users.noreply.github.com>
2021-02-23 16:05:42 -0600

    commit

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,41 +1,158 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-var button = d3.select("#filter-btn");
 
-button.on("click", function() {
-var Parent = document.getElementById("ufo-table");
+function appendTable(report) {
+    var tbody = d3.select("tbody");
 
-for(var i = Parent.rows.length - 1; i > 0; i--)
-{
-    Parent.deleteRow(i);
-};
+    var row = tbody.append("tr");
 
-var inputElement_date = d3.select("#datetime");
+  
+    Object.entries(report).forEach(([key, value]) => {
 
-var inputValue_date = inputElement_date.property("value");
+        
+        var cell = row.append("td");
+        cell.text(value);
 
-var filteredData = tableData.filter(tData => tData.datetime === inputValue_date);
-    console.log(filteredData);
+        
+        if (key === "comments") {
+            cell.attr("class", "record-comments")
+        }
+    });
+}
 
 
-var tbody = d3.select("tbody");
+tableData.forEach(appendTable);
 
 
-var table = d3.select("table");
+var submit = d3.select("#filter-btn");
 
-table.attr("class", "table table-striped");
+
+var countries = [];
+tableData.forEach(record => {
+    if (!(countries.includes(record.country))) {
+        countries.push(record.country)
+    }
+});
+var countryMenu = d3.select('#countryDropdown');
+countries.forEach(country => {
+    var item = countryMenu.append("option");
+    item.text(country.toUpperCase())
+})
+
+
+var shapes = [];
+tableData.forEach(record => {
+    if (!(shapes.includes(record.shape))) {
+        shapes.push(record.shape)
+    }
+});
+var shapeMenu = d3.select('#shapeDropdown');
+shapes.forEach(shape => {
+    var item = shapeMenu.append("option");
+    item.text(shape)
+})
+
+
+countryMenu.on('change', function(){
+
     
+    var selectedCountry = d3.select(this).property("value").toLowerCase();
+    // var selectedCountry = d3.event.target.value;
+  
+     var tableDataCountry = tableData.filter(record => record.country === selectedCountry);
+    var states = [];
+    tableDataCountry.forEach(record => {
+        if (!(states.includes(record.state))) {
+            states.push(record.state)
+        }
+    });
+    var stateMenu = d3.select('#stateDropdown');
+    stateMenu.html('<option class="optionPlaceholder">all</option>')
+    states.forEach(state => {
+        var item = stateMenu.append("option");
+        item.text(state.toUpperCase())
+    })
+    var cityMenu = d3.select('#cityDropdown');
+    cityMenu.html('<option class="optionPlaceholder">all</option>')
 
-filteredData.forEach((tabData) => {
+    
+    stateMenu.on('change', function(){
 
-  var row = tbody.append("tr");
-
- Object.entries(tabData).forEach(([key, value]) => {
-    var cell = row.append("td");
-    cell.text(value);
-  });
+       
+        var selectedState = d3.select(this).property("value").toLowerCase();
+    
+        
+        var tableDataState = tableDataCountry.filter(record => record.state === selectedState);
+        var cities = [];
+        tableDataState.forEach(record => {
+            if (!(cities.includes(record.city))) {
+                cities.push(record.city)
+            }
+        });
+        cityMenu.html('<option class="optionPlaceholder">all</option>')
+        cities.forEach(city => {
+            var item = cityMenu.append("option");
+            item.text(city)
+        })
+    });
 });
 
+// Filter data when clicking
+submit.on("click", function() {
+
+    // Remove existing table
+    d3.select("tbody").html("");
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Get the value property of the input element
+    var dateTime = d3.select("#datetime").property("value");
+    console.log(`Date: ${dateTime}`);
+    var selectedCountry = d3.select("#countryDropdown").property("value").toLowerCase();
+    console.log(`Country: ${selectedCountry}`);
+    var selectedState = d3.select("#stateDropdown").property("value").toLowerCase();
+    console.log(`State: ${selectedState}`);
+    var selectedCity = d3.select("#cityDropdown").property("value").toLowerCase();
+    console.log(`City: ${selectedCity}`);
+    var selectedShape = d3.select("#shapeDropdown").property("value").toLowerCase();
+    console.log(`Shape: ${selectedShape}`);
+
+    // Define filter function
+    function filterData(data, key, selectedValue) {
+        var filteredData = data.filter(record => 
+            record[key] === selectedValue
+        );
+        return filteredData
+    }
+
+    // Filter reports
+    var filteredData = tableData;
+    if (dateTime) {
+        filteredData = filterData(filteredData, 'datetime', dateTime)
+    }
+    if (selectedCountry !== "all") {
+        filteredData = filterData(filteredData, 'country', selectedCountry)
+    }
+    if (selectedState !== "all") {
+        filteredData = filterData(filteredData, 'state', selectedState)
+    }
+    if (selectedCity !== "all") {
+        filteredData = filterData(filteredData, 'city', selectedCity)
+    }
+    if (selectedShape !== "all") {
+        filteredData = filterData(filteredData, 'shape', selectedShape)
+    }
+
+    // Display the filtered dataset
+    var flag = d3.select("#no-matching-flag");
+    flag.html("");
+    if (filteredData.length === 0) {
+        var row = flag.append("h3");
+        row.text("No matching record.")
+    }
+    else {
+        filteredData.forEach(appendTable);
+    }
 });
